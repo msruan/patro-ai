63e91a59d0f076ec1462bb8b5b24620727376f49
Bianca <biancabezerrathe71@gmail.com>
2024-09-08 14:35:05 -0300

    feat: upload de foto pra api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,11 @@ import { ChatInput } from "@/components/ui/chat/chat-input";
 import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { api } from "@/config/api";
+import { chat } from "@/services/chat";
 import { MountJson } from "@/utils/mountJson";
 import { CornerDownLeft, SendHorizonal } from "lucide-react";
 import { useRef, useState } from "react";
-import { chat } from "@/services/chat";
 const ia =
   "https://i.pinimg.com/736x/8f/87/39/8f8739fbfae6ccde444f6bcd69007276.jpg";
 const asa =
@@ -29,20 +30,77 @@ export default function Home() {
   const [messages, setMessages] = useState([] as Message[]);
   const imageRef = useRef({} as HTMLInputElement);
 
+  function convertImageToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        const target = event.target as FileReader;
+        if (target.result) {
+          const base64String = (target.result as string).split(",")[1]; // Extrai a string Base64
+          resolve(base64String);
+        } else {
+          reject(new Error("Failed to read file"));
+        }
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+
+      reader.readAsDataURL(file); // Lê o arquivo como URL de dados
+    });
+  }
+
   async function handleImageSubmit() {
     if (imageRef.current.files) {
       const image = imageRef.current.files[0];
-      const res = await fetch("http://localhost:3000/api/image", {
-        body: image,
-        method: "POST",
-      });
-      console.log("RESPOSTA DA IMAGEM", res);
+      const url = await convertImageToBase64(image);
+      const value = inputRef.current.value;
+
+      if (value) {
+        const newMessages: Message[] = [
+          ...messages,
+          {
+            avatar_url: asa,
+            content: value,
+            timestamp: new Date(),
+            variant: "sent",
+          },
+        ];
+        setMessages(newMessages);
+
+        const formData = new FormData();
+        formData.append("file", url);
+        formData.append("prompt", value);
+        const res = await fetch("http://localhost:3000/api/image", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        const { text } = data;
+
+        setMessages((oldMessages) => [
+          ...oldMessages,
+          {
+            avatar_url: ia,
+            content: text,
+            timestamp: new Date(),
+            variant: "received",
+          },
+        ]);
+      }
     }
   }
 
   async function handleClick() {
     const value = inputRef.current.value;
-    console.log(messages);
+
     if (value) {
       const newMessages: Message[] = [
         ...messages,
@@ -58,6 +116,7 @@ export default function Home() {
       const body: { text: string } = await chat(
         JSON.stringify(MountJson(value, messages))
       );
+
       setMessages((oldValue) => [
         ...oldValue,
         {
@@ -91,29 +150,29 @@ export default function Home() {
           ref={inputRef}
           placeholder="Ask me anything..."
         />
-        <div className="flex flex-row">
+        <div className="flex flex-row w-full justify-between mb-12">
+          <div className="flex flex-row items-center">
+            <Label>Send picture</Label>
+            <Input id="picture" type="file" ref={imageRef} className="mr-2" />
+            <Button
+              onClick={handleImageSubmit}
+              size="default"
+              className="ml-auto h-10"
+            >
+              <SendHorizonal className="size-2.5" />
+            </Button>
+          </div>
+
           <div className="flex flex-row justify-center items-center h-10">
             <Button
               onClick={handleClick}
               size="default"
-              className="ml-auto gap-1.5 mb-12"
+              className="ml-auto gap-1.5"
             >
               Send Message
               <CornerDownLeft className="size-3.5" />
             </Button>
           </div>
-
-          {/* <div>
-            <Label>Send picture</Label>
-            <Input id="picture" type="file" ref={imageRef} />
-            <Button
-              onClick={handleImageSubmit}
-              size="default"
-              className="ml-2 h-10 w-10"
-            >
-              <SendHorizonal size={18} />
-            </Button>
-          </div> */}
         </div>
       </div>
     </div>
